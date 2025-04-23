60051f68cf40e5690b030bc2dabb0a0805b9d3d4
Haroldo Furtado <haroldofurtado@gmail.com>
2025-04-23 10:00:55 -0300

    refactor: streamline documentation formatter configuration in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,15 +51,14 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = :random
 
-  #   # Many RSpec users commonly either run the entire suite or an individual
-  #   # file, and it's useful to allow more verbose output when running an
-  #   # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
-  end
+  # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  #
+  # Use the documentation formatter for detailed output,
+  # unless a formatter has already been configured
+  # (e.g. via a command-line flag).
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
